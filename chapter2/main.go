f8caba5ab3d32615abe3a2eeae33570dd8e0a75a
lapeka.v <v.lapeka@andersenlab.com>
2024-10-28 23:30:53 +0100

    rewrite using go with gin

---
+++ chapter2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	goal := ""
+
+	r.GET("/", func(c *gin.Context) {
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		c.String(200, fmt.Sprintf(`
+			<!doctype html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport"
+					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>Document</title>
+			</head>
+			<body>
+				<p>%s</p>
+				<form method="post" action="/">
+					<input name="goal">
+					<button>Set goal</button>
+				</form>
+			</body>
+			</html>
+		`, goal))
+	})
+
+	r.POST("/", func(c *gin.Context) {
+		goal = c.PostForm("goal")
+		c.Redirect(http.StatusSeeOther, "/")
+	})
+
+	r.Run(":3000")
+}
